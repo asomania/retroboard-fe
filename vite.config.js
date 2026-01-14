a80544d2cd41b316ab16ef6a117ab40d6e761f2a
asomania <erenkucuk2002@gmail.com>
2026-01-14 23:29:23 +0300

    viteConfig fixed

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,26 @@
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
-    react(),
-    tailwindcss(),
-  ],
-  server: {
-    proxy: {
-      "/boards": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const apiUrl = env.VITE_API_URL || "http://localhost:3000";
+
+  return {
+    plugins: [
+      TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+      react(),
+      tailwindcss(),
+    ],
+    server: {
+      proxy: {
+        "/boards": {
+          target: apiUrl,
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
